a8f69375bbbd90ccb09dcb97ff5da11887f2d189
NilavroSeal2000 <seal.nilavro2000@gmail.com>
2022-02-24 20:13:46 +0530

    modified

--- src/lab2/Exercise4.java
+++ src/lab2/Exercise4.java
@@ -25,19 +25,26 @@ public class Exercise4 {
 					unique++;
 				}
 		}
-		int n = arr.length;
-		   int[] temp = new int[n];  
-	        int j = 0;  
-	        for (int i=0; i<n-1; i++){  
-	            if (arr[i] != arr[i+1]){  
-	                temp[j++] = arr[i];  
-	            }  
-	         }  
-	        temp[j++] = arr[n-1];     
-	        // Changing original array  
-	        for (int i=0; i<j; i++){  
-	            arr[i] = temp[i];  
-	        }  
+		
+		if(arr[arr.length - 1] != arr[arr.length - 2])
+		{
+			unique++;
+		}
+		
+		int j = 0;
+		int ans[] = new int[unique];
+		for(int i=0;i<arr.length - 1;i++)
+		{
+			if(arr[i] != arr[i+1])
+			{
+				ans[j++] = arr[i];
+			}
+		}
+		if(arr[arr.length - 1] != arr[arr.length - 2])
+		{
+			ans[j++] = arr[arr.length - 1];
+		}
+		return ans;
 	}
 
 	public static void main(String[] args) {
